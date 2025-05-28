da76c16852d3b9b28fd76265d5ba0ff257d58c6e
SaktiPadmayoga <sakti14yoga@gmail.com>
2025-05-29 00:51:08 +0700

    update route

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,41 @@
 import React from 'react'
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import {BrowserRouter as Router, Routes, Route, Outlet} from 'react-router-dom';
 import Register from './pages/Auth/Register';
 import Dashboard from './pages/User/Dashboard';
 import MyTasks from './pages/User/MyTasks';
 import MySchedule from './pages/User/MySchedule';
 import ViewTaskDetails from './pages/User/ViewTaskDetails';
+import Login from './pages/Auth/Login';
+import LandingNav from './pages/LandingPage/component/LandingNav';
+import Landing from './pages/LandingPage/pages/Landing';
+import UserProvider, { UserContext } from './context/userContext';
 
 const App = () => {
+//   const UserLayout = () => {
+//     return (
+//       <>
+//         <LandingNav />
+//         <Outlet />{}
+//       </>
+//     );
+// };
   return (
-    <div>
-      <Router>
-        <Routes>
-          <Route path='/login' element={<Login />}/>
-          <Route path='/register' element={<Register />}/>
-          <Route path='/dashboard' element={<Dashboard />}/>
-          <Route path='/mytasks' element={<MyTasks />}/>
-          <Route path='/myschedule' element={<MySchedule />}/>
-          <Route path='/task-details/:id' element={<ViewTaskDetails />}/>
-        </Routes>
-      </Router>
-    </div>
+    <UserProvider>
+      <div>
+        <Router>
+          <Routes>
+            <Route path='/login' element={<Login />}/>
+            <Route path='/register' element={<Register />}/>
+            <Route path='/' element={<Landing />}/>
+            <Route path='/dashboard' element={<Dashboard />}/>
+            <Route path='/mytasks' element={<MyTasks />}/>
+            <Route path='/myschedule' element={<MySchedule />}/>
+            <Route path='/task-details/:id' element={<ViewTaskDetails />}/>
+          </Routes>
+        </Router>
+      </div>
+    </UserProvider>
+    
   )
 }
 
